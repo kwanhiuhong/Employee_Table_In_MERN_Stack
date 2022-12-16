0c78062ac5e39b943d985d03faec01573b4b0319
Terry Kwan <hiuhongkwan@gmail.com>
2022-12-15 16:01:32 -0800

    Added "Add Employees" button UI

--- src/pages/EmployeeTable.js
+++ src/pages/EmployeeTable.js
@@ -4,8 +4,17 @@ import PopupFrom from "../components/PopupForm";
 import Dialog from "@mui/material/Dialog";
 import { EmployeeTableControllerContext } from "../controllers/EmployeeTableController";
 import Box from "@mui/material/Box";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  success: {
+    backgroundColor: theme.palette.success.main,
+  },
+}));
 
 const EmployeeTableView = () => {
+  const classes = useStyles();
   const context = useContext(EmployeeTableControllerContext);
   const {
     model: { shouldOpenPopupForm, employeesData },
@@ -16,8 +25,18 @@ const EmployeeTableView = () => {
       <Dialog open={shouldOpenPopupForm} onClose={handleClose}>
         <PopupFrom />
       </Dialog>
-      <Box m={2}>
+      <Box m={2} alignItems="flex-end">
         <StickyHeadTable data={employeesData} />
+        <Box
+          m={1}
+          display="flex"
+          justifyContent="flex-end"
+          alignItems="flex-end"
+        >
+          <Button variant="contained" classes={{ root: classes.success }}>
+            ADD EMPLOYEE
+          </Button>
+        </Box>
       </Box>
     </>
   );
